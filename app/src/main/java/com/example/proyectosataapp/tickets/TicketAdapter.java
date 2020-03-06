4c814f04217e70546e168ff65c1bff21ac19d876
Ajeky <alvaromarquez98@gmail.com>
2020-03-06 13:30:04 +0100

    El gif de carga de imagenes en las listas de tickets desaparecía antes de tiempo, solucionado

--- app/src/main/java/com/example/proyectosataapp/tickets/TicketAdapter.java
+++ app/src/main/java/com/example/proyectosataapp/tickets/TicketAdapter.java
@@ -81,7 +81,6 @@ public class TicketAdapter extends RecyclerView.Adapter<TicketAdapter.ViewHolder
             String url = holder.mItem.getFotos().get(0);
             String params[] = url.split("/");
 
-            holder.pbFoto.setVisibility(View.GONE);
             Toast.makeText(view.getContext(), "" + params[params.length - 2] + params[params.length - 1], Toast.LENGTH_SHORT).show();
 
             viewModel.getTicketImg(params[params.length - 2], params[params.length - 1]).observeForever(new Observer<ResponseBody>() {
@@ -98,6 +97,7 @@ public class TicketAdapter extends RecyclerView.Adapter<TicketAdapter.ViewHolder
                             //.load(responseBody)
                             .centerCrop()
                             .into(holder.ivFotoTicket);
+                    holder.pbFoto.setVisibility(View.GONE);
                 }
             });
         } else
